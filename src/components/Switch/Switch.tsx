27d46bae2bc753f076865d6632e40cbd408a6643
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-05 23:28:49 +0800

    normalise size

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -31,13 +31,13 @@ const Switch = ({
       ></input>
       <span className="h-9"></span>
       <span
-        className={`cursor-pointer relative w-7 h-3 mr-4 transition-colors rounded-full ${
+        className={`cursor-pointer relative ml-2 w-5 h-3 mr-4 transition-colors rounded-full ${
           fChecked ? 'bg-feli-light' : 'bg-gray-400'
         }`}
       >
         <span
-          className={`absolute inline-flex items-center justify-center transform -left-3 -top-3 transition-transform ${
-            fChecked ? 'translate-x-5' : ''
+          className={`absolute inline-flex items-center justify-center transform -left-4 -top-3 transition-transform ${
+            fChecked ? 'translate-x-4' : ''
           }`}
         >
           <Underlay>
