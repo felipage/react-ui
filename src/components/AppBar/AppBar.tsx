67d6ad7af2174aca1ca26d5454f18be2b49323be
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-05 23:38:41 +0800

    added option to define title href and onClick

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -5,15 +5,24 @@ export interface AppBarProps {
   title: string;
   Icon?: IconType;
   actions?: ReactNode;
+  onTitleClick?: () => void;
+  titleHref: string;
 }
 
-const AppBar = ({ title, actions = [], Icon }: AppBarProps) => {
+const AppBar = ({
+  title,
+  actions = [],
+  Icon,
+  onTitleClick = () => {},
+  titleHref,
+}: AppBarProps) => {
   return (
     <header className="flex items-center justify-center px-4 bg-white shadow-md min-h-56 md:min-h-64 dark:bg-black md:px-8">
       <div className="flex items-center justify-between w-full max-w-6xl">
         <a
+          onClick={onTitleClick}
           className="flex items-center justify-start space-x-3 text-gray-900 transition-shadow rounded focus:outline-none focus:ring-3 ring-feli dark:text-gray-50"
-          href="/"
+          href={titleHref}
         >
           {Icon && <Icon size={28} />}
           <span className="text-xl font-bold">{title}</span>
