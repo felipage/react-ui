78599b3c8017eec7a2c5021d81aa4945b24eb94f
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 01:49:10 +0800

    extends html div and forwards ref

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,20 +1,21 @@
-import React, { ReactNode } from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 import TooltipContainer from './TooltipContainer';
 
-export interface TooltipProps {
-  children: ReactNode;
+export interface TooltipProps extends Omit<HTMLProps<HTMLDivElement>, 'ref'> {
   label: string;
 }
 
-const Tooltip = ({ children, label }: TooltipProps) => {
-  return (
-    <div className="relative inline-block group">
-      {children}
-      <div className="absolute flex items-center justify-center transition-opacity transform -translate-x-1/2 translate-y-3 opacity-0 pointer-events-none left-1/2 group-hover:opacity-100">
-        <TooltipContainer>{label}</TooltipContainer>
+const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
+  ({ children, label, ...rest }: TooltipProps, ref) => {
+    return (
+      <div className="relative inline-block group" ref={ref} {...rest}>
+        {children}
+        <div className="absolute flex items-center justify-center transition-opacity transform -translate-x-1/2 translate-y-3 opacity-0 pointer-events-none left-1/2 group-hover:opacity-100">
+          <TooltipContainer>{label}</TooltipContainer>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Tooltip;
