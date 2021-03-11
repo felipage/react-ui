2f46d21cac1af40e86aa1eab6ec6c05fe507b059
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 01:44:01 +0800

    extends html span and forwards ref

--- src/components/Thumb/Thumb.tsx
+++ src/components/Thumb/Thumb.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 
-export interface ThumbProps {
+export interface ThumbProps extends Omit<HTMLProps<HTMLSpanElement>, 'ref'> {
   active?: boolean;
 }
 
-const Thumb = ({ active = false }: ThumbProps) => {
-  return (
-    <span
-      className={`flex items-center justify-center w-5 h-5 transition rounded-full shadow ${
-        active ? 'bg-feli' : 'bg-white'
-      }`}
-    ></span>
-  );
-};
+const Thumb = forwardRef<HTMLSpanElement, ThumbProps>(
+  ({ active = false, ...rest }: ThumbProps, ref) => {
+    return (
+      <span
+        className={`flex items-center justify-center w-5 h-5 transition rounded-full shadow ${
+          active ? 'bg-feli' : 'bg-white'
+        }`}
+        ref={ref}
+        {...rest}
+      ></span>
+    );
+  }
+);
 
 export default Thumb;
