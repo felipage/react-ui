9e292bf05998b6c8c24ae6df705617db633080b9
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 00:42:23 +0800

    added interminate

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 
 export interface ProgressBarProps {
   label: string;
-  progress: number;
+  progress?: number;
 }
 
-const ProgressBar = ({ progress = 0, label }: ProgressBarProps) => {
+const ProgressBar = ({ progress, label }: ProgressBarProps) => {
   return (
     <label className="flex flex-col items-center justify-center">
       <span>
-        {label} {(progress * 100).toFixed()}%
+        {label} {progress !== undefined && `${(progress * 100).toFixed()}%`}
       </span>
       <progress
-        value={(progress * 100).toFixed()}
+        value={progress && (progress * 100).toFixed()}
         max="100"
         className="w-0 h-0 opacity-0"
       ></progress>
       <span className="relative inline-block w-full h-1 max-w-lg overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
         <span
-          style={{ width: `${progress * 100}%` }}
-          className="absolute h-1 bg-feli"
+          style={
+            progress !== undefined
+              ? { width: `${progress * 100}%` }
+              : { width: '100%' }
+          }
+          className={`absolute h-1 bg-feli ${
+            progress === undefined ? 'animate-pulse' : ''
+          }`}
         ></span>
       </span>
     </label>
