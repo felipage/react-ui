18e2e1d31be89141814d2fe4cd4c39b3f6384cdf
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-11 21:12:37 +0800

    extends chip base and forwards ref

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,25 +1,24 @@
-import ChipBase from './ChipBase';
-import React, { ReactNode } from 'react';
+import ChipBase, { ChipBaseProps } from './ChipBase';
+import React, { forwardRef } from 'react';
 
-export interface ChipProps {
-  children: ReactNode;
+export interface ChipProps extends ChipBaseProps {
   active?: boolean;
-  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-const Chip = ({ children, active = false, onClick }: ChipProps) => {
-  return (
-    <ChipBase
-      onClick={onClick}
-      className={`${
-        active
-          ? 'bg-feli-lighter hover:bg-feli-light active:bg-feli text-black active:text-white'
-          : 'bg-gray-200 hover:bg-gray-300 active:bg-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 dark:active:bg-gray-500 text-black dark:text-white'
-      }`}
-    >
-      {children}
-    </ChipBase>
-  );
-};
+const Chip = forwardRef<HTMLDivElement, ChipProps>(
+  ({ active = false, ...rest }: ChipProps, ref) => {
+    return (
+      <ChipBase
+        className={`${
+          active
+            ? 'bg-feli-lighter hover:bg-feli-light active:bg-feli text-black active:text-white'
+            : 'bg-gray-200 hover:bg-gray-300 active:bg-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 dark:active:bg-gray-500 text-black dark:text-white'
+        }`}
+        ref={ref}
+        {...rest}
+      />
+    );
+  }
+);
 
 export default Chip;
