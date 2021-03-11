e843fabd99c01f904ff9283854e44bbb0cfcb3bf
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 00:24:02 +0800

    extends html button and forwards ref

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 import { IconType } from 'react-icons/lib';
 import { Underlay } from '../Underlay';
 
 // type sizes = 'small' | 'normal' | 'large';
 
-export interface IconButtonProps {
+export interface IconButtonProps
+  extends Omit<HTMLProps<HTMLButtonElement>, 'ref'> {
   Icon: IconType;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  type: 'button' | 'submit' | 'reset' | undefined;
 }
 
-const IconButton = ({ Icon, onClick }: IconButtonProps) => {
-  return (
-    <Underlay size="large">
-      <button
-        className="flex items-center justify-center w-full h-full focus:outline-none"
-        onClick={onClick}
-      >
-        <Icon size={24} />
-      </button>
-    </Underlay>
-  );
-};
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ Icon, ...rest }: IconButtonProps, ref) => {
+    return (
+      <Underlay size="large">
+        <button
+          className="flex items-center justify-center w-full h-full focus:outline-none"
+          ref={ref}
+          {...rest}
+        >
+          <Icon size={24} />
+        </button>
+      </Underlay>
+    );
+  }
+);
 
 export default IconButton;
