6487fb4d3d43cdb52312a058dc5baf4309847b0b
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-11 19:33:10 +0800

    forwards ref

--- src/components/Alert/AlertBase.tsx
+++ src/components/Alert/AlertBase.tsx
@@ -1,25 +1,26 @@
-import React, { HTMLProps } from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 
 export interface AlertBaseProps extends HTMLProps<HTMLDivElement> {
   fixed?: boolean;
 }
 
-const AlertBase = ({
-  children,
-  className = '',
-  fixed = true,
-  ...rest
-}: AlertBaseProps) => {
-  return (
-    <div
-      className={`w-full px-4 py-2 transition-colors shadow-md ${
-        fixed ? 'fixed bottom-0' : ''
-      } ${className}`}
-      {...rest}
-    >
-      {children}
-    </div>
-  );
-};
+const AlertBase = forwardRef<HTMLDivElement, AlertBaseProps>(
+  (
+    { children, className = '', fixed = true, ...rest }: AlertBaseProps,
+    ref
+  ) => {
+    return (
+      <div
+        ref={ref}
+        className={`w-full px-4 py-2 transition-colors shadow-md ${
+          fixed ? 'fixed bottom-0' : ''
+        } ${className}`}
+        {...rest}
+      >
+        {children}
+      </div>
+    );
+  }
+);
 
 export default AlertBase;
