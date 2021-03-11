183daa92708b9f9f751f08da2704df6f3fa1f30c
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 00:27:18 +0800

    extends html div and forwards ref

--- src/components/ListTile/ListTile.tsx
+++ src/components/ListTile/ListTile.tsx
@@ -1,37 +1,37 @@
-import React, { ReactNode } from 'react';
+import React, { forwardRef, HTMLProps, ReactNode } from 'react';
 
-export interface ListTileProps {
+export interface ListTileProps extends Omit<HTMLProps<HTMLDivElement>, 'ref'> {
   Leading?: ReactNode;
   Trailing?: ReactNode;
   title: string;
   subtitle?: string;
-  onClick?: () => void;
 }
 
-const ListTile = ({
-  Leading,
-  Trailing,
-  title,
-  subtitle,
-  onClick,
-}: ListTileProps) => {
-  return (
-    <div
-      onClick={onClick}
-      className={`h-16 flex items-center justify-center py-2 bg-white rounded-md dark:bg-black ${
-        onClick
-          ? 'cursor-pointer hover:bg-gray-100 active:bg-gray-200 dark:hover:bg-gray-900 dark:active:bg-gray-800 transition'
-          : ''
-      }`}
-    >
-      {Leading && <span className="ml-4">{Leading}</span>}
-      <div className="flex-grow mx-4">
-        <div className="text-base line-clamp-1">{title}</div>
-        <div className="text-sm line-clamp-1">{subtitle}</div>
+const ListTile = forwardRef<HTMLDivElement, ListTileProps>(
+  (
+    { Leading, Trailing, title, subtitle, onClick, ...rest }: ListTileProps,
+    ref
+  ) => {
+    return (
+      <div
+        onClick={onClick}
+        className={`h-16 flex items-center justify-center py-2 bg-white rounded-md dark:bg-black ${
+          onClick
+            ? 'cursor-pointer hover:bg-gray-100 active:bg-gray-200 dark:hover:bg-gray-900 dark:active:bg-gray-800 transition'
+            : ''
+        }`}
+        ref={ref}
+        {...rest}
+      >
+        {Leading && <span className="ml-4">{Leading}</span>}
+        <div className="flex-grow mx-4">
+          <div className="text-base line-clamp-1">{title}</div>
+          <div className="text-sm line-clamp-1">{subtitle}</div>
+        </div>
+        {Trailing && <span className="mr-4">{Trailing}</span>}
       </div>
-      {Trailing && <span className="mr-4">{Trailing}</span>}
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default ListTile;
