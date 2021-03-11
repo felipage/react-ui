14386886c64c23b0f26345a34fe8f9f77c2038ec
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 00:54:29 +0800

    extends html div and forwards ref

--- src/components/Switch/SwitchGroup.tsx
+++ src/components/Switch/SwitchGroup.tsx
@@ -1,59 +1,68 @@
 import Switch from './Switch';
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, {
+  forwardRef,
+  HTMLProps,
+  ReactNode,
+  useEffect,
+  useState,
+} from 'react';
 
 export interface SwitchGroupOptions {
   value: string;
   label: ReactNode;
 }
 
-export interface SwitchGroupProps {
+export interface SwitchGroupProps
+  extends Omit<HTMLProps<HTMLDivElement>, 'ref' | 'onChange'> {
   options: SwitchGroupOptions[];
   onChange?: (value: string[]) => void;
 }
 
-const SwitchGroup = ({ options, onChange = () => {} }: SwitchGroupProps) => {
-  const [fSelectedValues, fSetSelectedValues] = useState([
-    ...options.map(option => ({
-      value: option.value,
-      checked: false,
-    })),
-  ]);
+const SwitchGroup = forwardRef<HTMLDivElement, SwitchGroupProps>(
+  ({ options, onChange = () => {}, ...rest }: SwitchGroupProps, ref) => {
+    const [fSelectedValues, fSetSelectedValues] = useState([
+      ...options.map(option => ({
+        value: option.value,
+        checked: false,
+      })),
+    ]);
 
-  useEffect(() => {
-    onChange(
-      fSelectedValues
-        .filter(option => option.checked)
-        .map(option => option.value)
-    );
-  }, [fSelectedValues]);
+    useEffect(() => {
+      onChange(
+        fSelectedValues
+          .filter(option => option.checked)
+          .map(option => option.value)
+      );
+    }, [fSelectedValues]);
 
-  const handleChange = (value: string, checked: boolean) => {
-    console.log({ value, checked });
-    fSetSelectedValues(selectedValues => {
-      return selectedValues.map(values => {
-        if (values.value === value) return { value: values.value, checked };
-        return values;
+    const handleChange = (value: string, checked: boolean) => {
+      console.log({ value, checked });
+      fSetSelectedValues(selectedValues => {
+        return selectedValues.map(values => {
+          if (values.value === value) return { value: values.value, checked };
+          return values;
+        });
       });
-    });
-  };
+    };
 
-  return (
-    <div className="flex flex-col">
-      {options.map(option => {
-        const { value, label } = option;
-        return (
-          <Switch
-            checked={
-              fSelectedValues.find(option => option.value === value)!.checked
-            }
-            onChange={checked => handleChange(value, checked)}
-            key={value}
-            label={label}
-          />
-        );
-      })}
-    </div>
-  );
-};
+    return (
+      <div className="flex flex-col" ref={ref} {...rest}>
+        {options.map(option => {
+          const { value, label } = option;
+          return (
+            <Switch
+              checked={
+                fSelectedValues.find(option => option.value === value)!.checked
+              }
+              onChange={checked => handleChange(value, checked)}
+              key={value}
+              label={label}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+);
 
 export default SwitchGroup;
