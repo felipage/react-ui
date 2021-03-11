f322a50b0ce2447db57a35054542ad4eab051ae1
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-11 21:09:43 +0800

    extends html div and forwards ref

--- src/components/Chip/ChipBase.tsx
+++ src/components/Chip/ChipBase.tsx
@@ -1,20 +1,22 @@
-import React, { ReactNode } from 'react';
+import React, { forwardRef, HTMLProps, ReactNode } from 'react';
 
-interface Props {
+interface Props extends Omit<HTMLProps<HTMLDivElement>, 'ref'> {
   children: ReactNode;
   className?: string;
-  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-const ChipBase = ({ children, className = '', onClick = () => {} }: Props) => {
-  return (
-    <div
-      onClick={onClick}
-      className={`inline-block px-3.5 py-1 font-light rounded-full cursor-pointer transition-colors ${className}`}
-    >
-      {children}
-    </div>
-  );
-};
+const ChipBase = forwardRef<HTMLDivElement, Props>(
+  ({ children, className = '', ...rest }: Props, ref) => {
+    return (
+      <div
+        className={`inline-block px-3.5 py-1 font-light rounded-full cursor-pointer transition-colors ${className}`}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </div>
+    );
+  }
+);
 
 export default ChipBase;
