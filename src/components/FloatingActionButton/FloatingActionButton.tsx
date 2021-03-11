5462ea9348e43e4fb054b2385234bca10f61954e
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 00:23:59 +0800

    extends html button and forwards ref

--- src/components/FloatingActionButton/FloatingActionButton.tsx
+++ src/components/FloatingActionButton/FloatingActionButton.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 import { IconType } from 'react-icons';
 
 type sizes = 'small' | 'normal';
 
-export interface FloatingActionButtonProps {
+export interface FloatingActionButtonProps
+  extends Omit<HTMLProps<HTMLButtonElement>, 'ref' | 'size'> {
   Icon: IconType;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   size: sizes;
+  type: 'button' | 'submit' | 'reset' | undefined;
 }
 
-const FloatingActionButton = ({
-  Icon,
-  onClick,
-  size = 'normal',
-}: FloatingActionButtonProps) => {
+const FloatingActionButton = forwardRef<
+  HTMLButtonElement,
+  FloatingActionButtonProps
+>(({ Icon, size = 'normal', ...rest }: FloatingActionButtonProps, ref) => {
   return (
     <button
       className={`flex items-center justify-center transition rounded-full bg-feli hover:bg-feli-light active:bg-feli-lighter focus:outline-none ring-feli ring-offset-white hover:shadow-lg active:shadow-md ${
@@ -23,11 +23,12 @@ const FloatingActionButton = ({
           ? 'w-14 h-14 focus:ring-5 ring-offset-2.5'
           : ''
       }`}
-      onClick={onClick}
+      ref={ref}
+      {...rest}
     >
       <Icon size={size === 'small' ? 24 : size === 'normal' ? 32 : 0} />
     </button>
   );
-};
+});
 
 export default FloatingActionButton;
