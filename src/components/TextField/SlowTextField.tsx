80b07f44b6f06943a6c286a2ee081cb7d3c1facc
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 01:41:38 +0800

    forwards ref

--- src/components/TextField/SlowTextField.tsx
+++ src/components/TextField/SlowTextField.tsx
@@ -1,30 +1,35 @@
 import TextFieldBase, { TextFieldBaseProps } from './TextFieldBase';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { useEffect, useState } from 'react';
 
-export interface SlowTextFieldProps extends TextFieldBaseProps {}
+export interface SlowTextFieldProps extends TextFieldBaseProps {
+  value?: string;
+}
 
-const SlowTextField = ({
-  value,
-  onChange = () => {},
-  ...rest
-}: SlowTextFieldProps) => {
-  const [localValue, setLocalValue] = useState('');
+const SlowTextField = forwardRef<HTMLInputElement, SlowTextFieldProps>(
+  ({ value, onChange = () => {}, ...rest }: SlowTextFieldProps, ref) => {
+    const [localValue, setLocalValue] = useState(value ?? '');
 
-  useEffect(() => {
-    const timeout = setTimeout(() => onChange(localValue), 500);
-    return () => clearTimeout(timeout);
-  }, [localValue, onChange]);
+    useEffect(() => {
+      const timeout = setTimeout(() => onChange(localValue), 500);
+      return () => clearTimeout(timeout);
+    }, [localValue, onChange]);
 
-  useEffect(() => setLocalValue(value ?? ''), [value]);
+    useEffect(() => setLocalValue(value ?? ''), [value]);
 
-  const onValueChange = (value: string) => {
-    setLocalValue(value);
-  };
+    const onValueChange = (value: string) => {
+      setLocalValue(value);
+    };
 
-  return (
-    <TextFieldBase onChange={onValueChange} value={localValue} {...rest} />
-  );
-};
+    return (
+      <TextFieldBase
+        onChange={onValueChange}
+        value={localValue}
+        ref={ref}
+        {...rest}
+      />
+    );
+  }
+);
 
 export default SlowTextField;
