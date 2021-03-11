6f4230d41257ed6d33a58ed5528241798908f28b
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-11 19:58:18 +0800

    forwards ref

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,5 +1,5 @@
 import AlertBase, { AlertBaseProps } from './AlertBase';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 type colours = 'red' | 'green' | 'default';
 
@@ -7,23 +7,26 @@ export interface AlertProps extends AlertBaseProps {
   colour?: colours;
 }
 
-const Alert = ({ children, colour = 'default', ...rest }: AlertProps) => {
-  return (
-    <AlertBase
-      className={
-        colour === 'default'
-          ? 'text-gray-700 bg-gray-400 dark:bg-gray-900 dark:text-gray-200'
-          : colour === 'green'
-          ? 'text-green-700 bg-green-400 dark:bg-green-900 dark:text-green-200'
-          : colour === 'red'
-          ? 'text-red-700 bg-red-400 dark:bg-red-900 dark:text-red-200'
-          : ''
-      }
-      {...rest}
-    >
-      {children}
-    </AlertBase>
-  );
-};
+const Alert = forwardRef<HTMLDivElement, AlertProps>(
+  ({ children, colour = 'default', ...rest }: AlertProps, ref) => {
+    return (
+      <AlertBase
+        className={
+          colour === 'default'
+            ? 'text-gray-700 bg-gray-400 dark:bg-gray-900 dark:text-gray-200'
+            : colour === 'green'
+            ? 'text-green-700 bg-green-400 dark:bg-green-900 dark:text-green-200'
+            : colour === 'red'
+            ? 'text-red-700 bg-red-400 dark:bg-red-900 dark:text-red-200'
+            : ''
+        }
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </AlertBase>
+    );
+  }
+);
 
 export default Alert;
