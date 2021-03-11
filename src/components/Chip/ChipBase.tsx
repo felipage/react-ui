23e1dbc93876f51e292eb02e92c517da360cebca
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-11 21:10:02 +0800

    renamed interface and exports it

--- src/components/Chip/ChipBase.tsx
+++ src/components/Chip/ChipBase.tsx
@@ -1,12 +1,12 @@
 import React, { forwardRef, HTMLProps, ReactNode } from 'react';
 
-interface Props extends Omit<HTMLProps<HTMLDivElement>, 'ref'> {
+export interface ChipBaseProps extends Omit<HTMLProps<HTMLDivElement>, 'ref'> {
   children: ReactNode;
   className?: string;
 }
 
-const ChipBase = forwardRef<HTMLDivElement, Props>(
-  ({ children, className = '', ...rest }: Props, ref) => {
+const ChipBase = forwardRef<HTMLDivElement, ChipBaseProps>(
+  ({ children, className = '', ...rest }: ChipBaseProps, ref) => {
     return (
       <div
         className={`inline-block px-3.5 py-1 font-light rounded-full cursor-pointer transition-colors ${className}`}
