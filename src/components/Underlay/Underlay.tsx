1c0db111bd3bea6b02fc2849b308619b7c453185
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 01:50:40 +0800

    extends html span and forwards ref

--- src/components/Underlay/Underlay.tsx
+++ src/components/Underlay/Underlay.tsx
@@ -1,28 +1,30 @@
-import React, { ReactNode } from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 
 type sizes = 'small' | 'normal' | 'large';
 
-export interface UnderlayProps {
+export interface UnderlayProps
+  extends Omit<HTMLProps<HTMLSpanElement>, 'ref' | 'size'> {
   size?: sizes;
-  children?: ReactNode;
 }
 
-const Underlay = ({ size = 'normal', children }: UnderlayProps) => {
-  return (
-    <span
-      className={`inline-flex items-center justify-center hover:bg-feli-opacity-400 active:bg-feli-opacity-600 focus:bg-feli-opacity-400 rounded-full transition-colors ${
-        size === 'small'
-          ? 'w-6 h-6'
-          : size === 'normal'
-          ? 'w-9 h-9'
-          : size === 'large'
-          ? 'w-12 h-12'
-          : ''
-      }`}
-    >
-      {children}
-    </span>
-  );
-};
+const Underlay = forwardRef<HTMLSpanElement, UnderlayProps>(
+  ({ size = 'normal', ...rest }: UnderlayProps, ref) => {
+    return (
+      <span
+        className={`inline-flex items-center justify-center hover:bg-feli-opacity-400 active:bg-feli-opacity-600 focus:bg-feli-opacity-400 rounded-full transition-colors ${
+          size === 'small'
+            ? 'w-6 h-6'
+            : size === 'normal'
+            ? 'w-9 h-9'
+            : size === 'large'
+            ? 'w-12 h-12'
+            : ''
+        }`}
+        ref={ref}
+        {...rest}
+      ></span>
+    );
+  }
+);
 
 export default Underlay;
