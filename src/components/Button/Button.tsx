9d109f67f053b5200b11fa3d62b2a0178ccf0c1a
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-11 20:14:17 +0800

    extends html button and forwards ref

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,36 +1,28 @@
-import ButtonBase, { buttonSizes } from './ButtonBase';
-import React, { ReactNode } from 'react';
+import ButtonBase, { ButtonBaseProps } from './ButtonBase';
+import React, { forwardRef } from 'react';
 
 type colours = 'red' | 'default';
 
-export interface ButtonProps {
-  children: ReactNode;
+export interface ButtonProps extends ButtonBaseProps {
   colour?: colours;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  size?: buttonSizes;
 }
 
-const Button = ({
-  children,
-  colour = 'default',
-  onClick,
-  size,
-}: ButtonProps) => {
-  return (
-    <ButtonBase
-      size={size}
-      onClick={onClick}
-      className={`shadow hover:shadow-lg ${
-        colour === 'default'
-          ? 'text-black bg-gray-200 dark:text-white dark:bg-gray-800 dark:hover:bg-gray-600 dark:active:bg-gray-700 hover:bg-gray-400 active:bg-gray-300'
-          : colour === 'red'
-          ? 'text-white bg-red-600 hover:bg-red-800 active:bg-red-700'
-          : ''
-      }`}
-    >
-      {children}
-    </ButtonBase>
-  );
-};
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ colour = 'default', ...rest }: ButtonProps, ref) => {
+    return (
+      <ButtonBase
+        className={`shadow hover:shadow-lg ${
+          colour === 'default'
+            ? 'text-black bg-gray-200 dark:text-white dark:bg-gray-800 dark:hover:bg-gray-600 dark:active:bg-gray-700 hover:bg-gray-400 active:bg-gray-300'
+            : colour === 'red'
+            ? 'text-white bg-red-600 hover:bg-red-800 active:bg-red-700'
+            : ''
+        }`}
+        ref={ref}
+        {...rest}
+      />
+    );
+  }
+);
 
 export default Button;
--- src/components/Button/ButtonBase.tsx
+++ src/components/Button/ButtonBase.tsx
@@ -1,36 +1,31 @@
-import React, { ReactNode } from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 
 export type buttonSizes = 'small' | 'normal' | 'large';
 
-export interface ButtonBaseProps {
-  children?: ReactNode;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  className?: string;
+export interface ButtonBaseProps
+  extends Omit<HTMLProps<HTMLButtonElement>, 'size' | 'ref'> {
   size?: buttonSizes;
+  type?: 'button' | 'submit' | 'reset' | undefined;
 }
 
-const Button = ({
-  children,
-  onClick = () => {},
-  className = '',
-  size = 'normal',
-}: ButtonBaseProps) => {
-  return (
-    <button
-      className={`px-4 py-2 font-semibold text-black uppercase transition rounded select-none focus:outline-none ring-feli focus:ring-3 overflow-ellipsis overflow-hidden whitespace-nowrap ${
-        size === 'small'
-          ? 'text-xs h-8 tracking-wide'
-          : size === 'normal'
-          ? 'text-sm h-10 tracking-wider'
-          : size === 'large'
-          ? 'text-base h-12 tracking-widest'
-          : ''
-      } ${className}`}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = forwardRef<HTMLButtonElement, ButtonBaseProps>(
+  ({ className = '', size = 'normal', ...rest }: ButtonBaseProps, ref) => {
+    return (
+      <button
+        className={`px-4 py-2 font-semibold text-black uppercase transition rounded select-none focus:outline-none ring-feli focus:ring-3 overflow-ellipsis overflow-hidden whitespace-nowrap ${
+          size === 'small'
+            ? 'text-xs h-8 tracking-wide'
+            : size === 'normal'
+            ? 'text-sm h-10 tracking-wider'
+            : size === 'large'
+            ? 'text-base h-12 tracking-widest'
+            : ''
+        } ${className}`}
+        ref={ref}
+        {...rest}
+      />
+    );
+  }
+);
 
 export default Button;
