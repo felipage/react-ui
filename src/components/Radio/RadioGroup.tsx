bcf890e2246dea082698fde94333d4572507634a
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 00:49:18 +0800

    extends html div and forwards ref

--- src/components/Radio/RadioGroup.tsx
+++ src/components/Radio/RadioGroup.tsx
@@ -1,45 +1,44 @@
 import Radio from './Radio';
-import React, { ReactNode, useState } from 'react';
+import React, { forwardRef, HTMLProps, ReactNode, useState } from 'react';
 
 export interface RadioGroupOptions {
   value: string;
   label: ReactNode;
 }
 
-export interface RadioGroupProps {
+export interface RadioGroupProps
+  extends Omit<HTMLProps<HTMLDivElement>, 'ref' | 'onChange'> {
   name: string;
   options: RadioGroupOptions[];
   onChange?: (value: string) => void;
 }
 
-const RadioGroup = ({
-  options,
-  name,
-  onChange = () => {},
-}: RadioGroupProps) => {
-  const [fSelectedValue, fSetSelectedValue] = useState('');
+const RadioGroup = forwardRef<HTMLDivElement, RadioGroupProps>(
+  ({ options, name, onChange = () => {}, ...rest }: RadioGroupProps, ref) => {
+    const [fSelectedValue, fSetSelectedValue] = useState('');
 
-  const handleChange = (newValue: string) => {
-    onChange(newValue);
-    fSetSelectedValue(newValue);
-  };
+    const handleChange = (newValue: string) => {
+      onChange(newValue);
+      fSetSelectedValue(newValue);
+    };
 
-  return (
-    <div className="flex flex-col">
-      {options.map(option => {
-        const { value, label } = option;
-        return (
-          <Radio
-            key={value}
-            name={name}
-            checked={fSelectedValue === value}
-            onChange={checked => checked && handleChange(value)}
-            label={label}
-          />
-        );
-      })}
-    </div>
-  );
-};
+    return (
+      <div className="flex flex-col" ref={ref} {...rest}>
+        {options.map(option => {
+          const { value, label } = option;
+          return (
+            <Radio
+              key={value}
+              name={name}
+              checked={fSelectedValue === value}
+              onChange={checked => checked && handleChange(value)}
+              label={label}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+);
 
 export default RadioGroup;
