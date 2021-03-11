ec9325131a3e29d7fb524ab1c0c4bed19f265b11
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 00:59:31 +0800

    extends html select and forwards ref

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,47 +1,34 @@
-import React, { ReactNode } from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 import { HiOutlineArrowDown } from 'react-icons/hi';
 
-export interface SelectProps {
-  children: ReactNode;
-  id?: string;
+export interface SelectProps extends Omit<HTMLProps<HTMLSelectElement>, 'ref'> {
   label: string;
-  value?: string;
-  placeholder?: string;
-  disabled?: boolean;
 }
 
-const Select = ({
-  children,
-  id,
-  label,
-  value,
-  placeholder,
-  disabled = false,
-}: SelectProps) => {
-  const onChange = (value: string) => {
-    console.log(value);
-  };
+const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({ id, label, ...rest }: SelectProps, ref) => {
+    const onChange = (value: string) => {
+      console.log(value);
+    };
 
-  return (
-    <label htmlFor={id} className="inline-flex flex-col">
-      <span className="">{label}</span>
-      <span className="relative flex">
-        <select
-          onChange={e => onChange(e.target.value)}
-          value={value}
-          className="py-2 pl-4 pr-8 transition bg-gray-200 rounded-md appearance-none dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-800 dark:focus:bg-gray-800 focus:bg-gray-300 focus:outline-none ring-feli focus:ring-3"
-          id={id}
-          placeholder={placeholder}
-          disabled={disabled}
-        >
-          {children}
-        </select>
-        <span className="absolute pointer-events-none right-3 top-3">
-          <HiOutlineArrowDown size={16} />
+    return (
+      <label htmlFor={id} className="inline-flex flex-col">
+        <span className="">{label}</span>
+        <span className="relative flex">
+          <select
+            onChange={e => onChange(e.target.value)}
+            className="py-2 pl-4 pr-8 transition bg-gray-200 rounded-md appearance-none dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-800 dark:focus:bg-gray-800 focus:bg-gray-300 focus:outline-none ring-feli focus:ring-3"
+            id={id}
+            ref={ref}
+            {...rest}
+          ></select>
+          <span className="absolute pointer-events-none right-3 top-3">
+            <HiOutlineArrowDown size={16} />
+          </span>
         </span>
-      </span>
-    </label>
-  );
-};
+      </label>
+    );
+  }
+);
 
 export default Select;
