e72463c9d8decfa9ef8fed2f7b293e78adc57a53
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 01:09:02 +0800

    forwards ref

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -1,25 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
 import SnackbarBase, { SnackbarBaseProps } from './SnackbarBase';
 
 export interface SnackbarProps extends Omit<SnackbarBaseProps, 'show'> {}
 
-const Snackbar = ({ children, actions }: SnackbarProps) => {
-  const [show, setShow] = useState(true);
+const Snackbar = forwardRef<HTMLDivElement, SnackbarProps>(
+  ({ actions, ...rest }: SnackbarProps, ref) => {
+    const [show, setShow] = useState(true);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShow(false);
-    }, 5000);
-    return () => {
-      timeout && clearTimeout(timeout);
-    };
-  }, []);
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        setShow(false);
+      }, 5000);
+      return () => {
+        timeout && clearTimeout(timeout);
+      };
+    }, []);
 
-  return (
-    <SnackbarBase actions={actions} show={show}>
-      {children}
-    </SnackbarBase>
-  );
-};
+    return <SnackbarBase actions={actions} show={show} ref={ref} {...rest} />;
+  }
+);
 
 export default Snackbar;
