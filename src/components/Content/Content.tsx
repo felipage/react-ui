4b81616f79e026d948fce5698535221cd8372e59
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-11 21:20:32 +0800

    extends html element and forwards ref

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,15 +1,19 @@
-import React, { ReactNode } from 'react';
+import React, { forwardRef, HTMLProps } from 'react';
 
-export interface ContentProps {
-  children: ReactNode;
-}
+export interface ContentProps extends Omit<HTMLProps<HTMLElement>, 'ref'> {}
 
-const Content = ({ children }: ContentProps) => {
-  return (
-    <main className="flex justify-center w-full px-4 md:px-8">
-      <div className="w-full max-w-6xl">{children}</div>
-    </main>
-  );
-};
+const Content = forwardRef<HTMLElement, ContentProps>(
+  ({ children, ...rest }: ContentProps, ref) => {
+    return (
+      <main
+        className="flex justify-center w-full px-4 md:px-8"
+        ref={ref}
+        {...rest}
+      >
+        <div className="w-full max-w-6xl">{children}</div>
+      </main>
+    );
+  }
+);
 
 export default Content;
