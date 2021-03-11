c4cc21a34fe9e2bdee56bafd1e41388a8d9ec94d
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-11 19:32:58 +0800

    extends div props

--- src/components/Alert/AlertBase.tsx
+++ src/components/Alert/AlertBase.tsx
@@ -1,8 +1,6 @@
-import React, { ReactNode } from 'react';
+import React, { HTMLProps } from 'react';
 
-export interface AlertBaseProps {
-  children: ReactNode;
-  className?: string;
+export interface AlertBaseProps extends HTMLProps<HTMLDivElement> {
   fixed?: boolean;
 }
 
@@ -10,12 +8,14 @@ const AlertBase = ({
   children,
   className = '',
   fixed = true,
+  ...rest
 }: AlertBaseProps) => {
   return (
     <div
       className={`w-full px-4 py-2 transition-colors shadow-md ${
         fixed ? 'fixed bottom-0' : ''
       } ${className}`}
+      {...rest}
     >
       {children}
     </div>
