0ca23cfa67281ad0d8b0e40a078dc4b6f14e1c2a
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-16 09:24:51 +0800

    min height screen

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,10 +3,10 @@ import React, { forwardRef, HTMLProps } from 'react';
 export interface ContentProps extends Omit<HTMLProps<HTMLElement>, 'ref'> {}
 
 const Content = forwardRef<HTMLElement, ContentProps>(
-  ({ children, ...rest }: ContentProps, ref) => {
+  ({ children, className, ...rest }: ContentProps, ref) => {
     return (
       <main
-        className="flex justify-center w-full px-4 md:px-8"
+        className={`flex justify-center w-full min-h-screen px-4 md:px-8 ${className}`}
         ref={ref}
         {...rest}
       >
