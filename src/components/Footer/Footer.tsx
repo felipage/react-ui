880dfdeeb42791b42117d60847de516d6fcd14a9
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-04-16 10:10:09 +0800

    removed ugly shadow

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,14 +3,10 @@ import React, { forwardRef, HTMLProps } from 'react';
 export interface FooterProps extends Omit<HTMLProps<HTMLDivElement>, 'ref'> {}
 
 const Footer = forwardRef<HTMLDivElement, FooterProps>(
-  ({ children, className = '', ...rest }: FooterProps, ref) => {
+  ({ children, ...rest }: FooterProps, ref) => {
     return (
-      <div
-        ref={ref}
-        {...rest}
-        className={`transform rotate-180 shadow ${className}`}
-      >
-        <div className="flex justify-center transform rotate-180 bg-gray-100 dark:bg-black">
+      <div ref={ref} {...rest}>
+        <div className="flex justify-center bg-gray-100 dark:bg-black">
           <div className="justify-around w-full max-w-6xl mx-2 my-10 space-y-6 text-center md:space-y-0 md:text-left md:flex">
             {children}
           </div>
